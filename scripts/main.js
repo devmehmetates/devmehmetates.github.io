0e9b1fac8dbbeba51219628eb6c1ee0453da10a9
Mehmet ateş <Mehmet1095@icloud.com>
2022-03-12 02:22:07 +0300

    js started

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+function myFunction(color) {
+    var element = document.getElementById("demo");
+    if(color === 1){
+        element.style.color = "red";
+    }else if(color == 2){
+        element.style.color = "blue";
+    }else if(color == 3){
+        element.style.color = "green";
+    }else if(color == 4){
+        element.style.color = "yellow";
+    }else if(color == 5){
+        element.style.color = "pink";
+    }
+    
+   
+};
